8f85960d34824cf76704aa6e5552138ecfb8cd63
Ayush Manoj <ayushmanoj04@gmail.com>
2023-05-29 12:09:13 -0500

    Testing access token retrieval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,22 +22,22 @@ const circularMedium = localFont({
 });
 
 export default function Home() {
+  const [authorized, setAuthorized] = useState(false);
   const [accessToken, setAccessToken] = useState("");
 
   useEffect(() => {
-    console.log('hello')
-    setAccessToken(getAccessToken());
-  }, [accessToken]);
+    console.log("hello");
+    getAccessToken();
+  });
 
   function getAccessToken() {
-    let accessToken = "";
-    const queryString = window.location.search;
-    if (queryString.length > 0) {
-      const urlParams = new URLSearchParams(queryString);
-      accessToken = urlParams.get("access_token") ?? "";
+    const hashString = window.location.search;
+    if (hashString) {
+      let start = hashString.indexOf("=") + 1;
+      let end = hashString.indexOf("&");
+      setAccessToken(hashString.substring(start, end));
+      setAuthorized(true);
     }
-    console.log(accessToken);
-    return accessToken;
   }
 
   const client_id = "adfd1bc87db44b8898bfab14d45d4a6f";
@@ -50,32 +50,37 @@ export default function Home() {
   redirectURL += "&client_id=" + encodeURIComponent(client_id);
   redirectURL += "&scope=" + encodeURIComponent(scope);
   redirectURL += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+  redirectURL += "&show_dialog=true";
 
   return (
     <>
       <title>Explorify</title>
       <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-teal-200 to-lime-200 dark:from-teal-800 dark:to-lime-800 md:p-24 p-12">
-        <h1 className="text-8xl font-bold">Explorify</h1>
-        <h2 className="flex flex-col text-xl text-center my-12 gap-1">
-          <p className={circularBook.className}>
-            Explorify utilizes your Spotify listening history to generate
-            personalized recommendations for genres and artists you love.
-          </p>
-          <p className={circularMedium.className}>
-            Say goodbye to endless searching and experience the joy of
-            effortless musical exploration.
-          </p>
-        </h2>
-        <a
-          href={redirectURL}
-          className="rounded-full px-12 py-6 bg-green-500 hover:scale-105 duration-300 shadow-lg"
-        >
-          <h2 className="flex flex-row items-center gap-4 text-xl">
-            <IconBrandSpotify size={36} />
-            <p className={circularMedium.className}>Log in with Spotify</p>
-          </h2>
-        </a>
-        <p>{accessToken}</p>
+        <h1 className="sm:text-8xl text-7xl font-bold">Explorify</h1>
+        {!authorized && (
+          <>
+            <h2 className="flex flex-col text-xl text-center my-16 gap-1">
+              <p>
+                Explorify utilizes your Spotify listening history to generate
+                personalized recommendations for genres and artists you love.
+              </p>
+              <p className="font-bold">
+                Say goodbye to endless searching and experience the joy of
+                effortless musical exploration.
+              </p>
+            </h2>
+            <a
+              href={redirectURL}
+              className="rounded-full px-10 py-6 bg-green-500 hover:scale-105 duration-300 shadow-lg"
+            >
+              <h2 className="flex flex-row items-center gap-3 sm:text-xl text-lg">
+                <IconBrandSpotify size={36} />
+                <p className={circularMedium.className}>Log in with Spotify</p>
+              </h2>
+            </a>
+          </>
+        )}
+        {authorized && <p>{accessToken}</p>}
       </main>
     </>
   );
